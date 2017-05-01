d246501c664f7b1963d8767fe213c722b60ba4f5
muhdmirzamz <muhdmirzamz@gmail.com>
2017-05-01 19:09:21 +0800

    Removed iOS component

--- ios/Terawhere/Pods/Target Support Files/Pods-Terawhere/Pods-Terawhere-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_TerawhereVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_TerawhereVersionString[];
-
--- ios/Terawhere/Pods/Target Support Files/Pods-TerawhereTests/Pods-TerawhereTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TerawhereTests : NSObject
-@end
-@implementation PodsDummy_Pods_TerawhereTests
-@end
