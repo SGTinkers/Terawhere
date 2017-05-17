68c48b88349b048b5df1012c3d2c024123e2a35f
saifuls <saifulss85@gmail.com>
2017-05-17 20:22:06 +0800

    Cleaned up MainActivity - app now loads straight into the Home tab.

--- android/Terawhere/app/src/main/java/tech/msociety/terawhere/globals/Constants.java
+++ android/Terawhere/app/src/main/java/tech/msociety/terawhere/globals/Constants.java
@@ -3,7 +3,8 @@ package tech.msociety.terawhere.globals;
 import tech.msociety.terawhere.TerawhereApplication;
 
 public class Constants {
-    public static final String BASE_URL = "https://api.terawhere.com"; // 139.59.224.66
+    //    public static final String BASE_URL = "https://api.terawhere.com";  // prod
+    public static final String BASE_URL = "http://139.59.224.66";   // dev
 
     private static String bearerToken;
 
