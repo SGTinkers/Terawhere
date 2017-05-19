79e59f189d54de98d88beac9eae9d66f7c4bf570
saifuls <saifulss85@gmail.com>
2017-05-19 15:08:31 +0800

    Copied over icons from Zeplin - for all provided resolutions. It's still looking fuzzy on my 5.5" OnePlus 3.

--- android/Terawhere/app/src/main/java/tech/msociety/terawhere/adapters/OffersAdapter.java
+++ android/Terawhere/app/src/main/java/tech/msociety/terawhere/adapters/OffersAdapter.java
@@ -34,23 +34,22 @@ public class OffersAdapter extends RecyclerView.Adapter<OffersAdapter.ViewHolder
     private List<Offer> offers;
     private ViewGroup viewGroup;
     private String[] value = new String[]{"Edit", "Delete"};
-
+    
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_offer, parent, false);
         viewGroup = parent;
-
+        
         return new ViewHolder(view);
     }
-
+    
     @Override
     public void onBindViewHolder(final ViewHolder viewHolder, final int position) {
         final Offer offer = offers.get(position);
-
-
+        
         viewHolder.endingLocationTextView.setText("Destination: " + offer.getEndingLocationAddress());
         viewHolder.startingLocationTextView.setText("Meeting Point: " + offer.getStartingLocationAddress());
-
+        
         String meetUpTime = "";
         String day = "";
         String month = "";
@@ -58,8 +57,7 @@ public class OffersAdapter extends RecyclerView.Adapter<OffersAdapter.ViewHolder
             meetUpTime = new SimpleDateFormat("hh:mm a").format(new SimpleDateFormat("yyyy-dd-MM HH:mm:ss").parse(offer.getMeetUpTime().toString()));
             day = new SimpleDateFormat("dd").format(new SimpleDateFormat("yyyy-dd-MM HH:mm:ss").parse(offer.getMeetUpTime().toString()));
             month = new SimpleDateFormat("MMMM").format(new SimpleDateFormat("yyyy-dd-MM HH:mm:ss").parse(offer.getMeetUpTime().toString()));
-
-
+    
         } catch (ParseException e) {
             e.printStackTrace();
         }
@@ -68,13 +66,13 @@ public class OffersAdapter extends RecyclerView.Adapter<OffersAdapter.ViewHolder
         }
         viewHolder.dayTextView.setText(day);
         viewHolder.monthTextView.setText(month);
-
+        
         if (offer.getDriverRemarks().matches("")) {
             viewHolder.remarksTextView.setText("Remarks: NIL");
         } else {
             viewHolder.remarksTextView.setText("Remarks: " + offer.getDriverRemarks());
         }
-
+        
         final boolean[] shouldExpand = {viewHolder.remarksTextView.getVisibility() == View.GONE};
         ChangeBounds transition = new ChangeBounds();
         transition.setDuration(125);
@@ -84,42 +82,42 @@ public class OffersAdapter extends RecyclerView.Adapter<OffersAdapter.ViewHolder
                 if (shouldExpand[0]) {
                     viewHolder.remarksTextView.setVisibility(View.VISIBLE);
                     viewHolder.detailsTextView.setText("\u2014 LESS DETAILS");
-
+    
                     shouldExpand[0] = false;
                 } else {
                     viewHolder.remarksTextView.setVisibility(View.GONE);
                     viewHolder.detailsTextView.setText("+ MORE DETAILS");
-
+    
                     shouldExpand[0] = true;
                 }
-
+    
                 TransitionManager.beginDelayedTransition(viewGroup);
                 viewHolder.itemView.setActivated(shouldExpand[0]);
-
+    
             }
         });
-
+        
         viewHolder.detailsTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (shouldExpand[0]) {
                     viewHolder.remarksTextView.setVisibility(View.VISIBLE);
                     viewHolder.detailsTextView.setText("\u2014 LESS DETAILS");
-
+    
                     shouldExpand[0] = false;
                 } else {
                     viewHolder.remarksTextView.setVisibility(View.GONE);
                     viewHolder.detailsTextView.setText("+ MORE DETAILS");
-
+    
                     shouldExpand[0] = true;
                 }
-
+    
                 TransitionManager.beginDelayedTransition(viewGroup);
                 viewHolder.itemView.setActivated(shouldExpand[0]);
-
+    
             }
         });
-
+        
         viewHolder.editButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -130,42 +128,42 @@ public class OffersAdapter extends RecyclerView.Adapter<OffersAdapter.ViewHolder
                 intent.putExtra("driverId", offer.getDriverId());
                 Log.i("DRIVER", ":" + offer.getDriverId());
                 intent.putExtra("meetUpTime", offer.getMeetUpTime());
-
+    
                 intent.putExtra("startingLocationName", offer.getStartingLocationName());
                 intent.putExtra("startingLocationAddress", offer.getStartingLocationAddress());
                 intent.putExtra("startingLocationLatitude", offer.getStartingLocationLatitude());
                 intent.putExtra("startingLocationLongitude", offer.getStartingLocationLongitude());
-
+    
                 intent.putExtra("endingLocationName", offer.getEndingLocationName());
                 intent.putExtra("endingLocationAddress", offer.getEndingLocationAddress());
                 intent.putExtra("endingLocationLatitude", offer.getEndingLocationLatitude());
                 intent.putExtra("endingLocationLongitude", offer.getEndingLocationLongitude());
-
+    
                 Log.i("LAT2", ":" + offer.getEndingLocationLatitude());
                 Log.i("LON2", ":" + offer.getEndingLocationLongitude());
-
+    
                 intent.putExtra("driverRemarks", offer.getDriverRemarks());
                 intent.putExtra("seatsAvailable", offer.getSeatsAvailable());
-
+    
                 intent.putExtra("endingLocationAddress", offer.getEndingLocationAddress());
                 intent.putExtra("endingLocationAddress", offer.getEndingLocationAddress());
-
+    
                 intent.putExtra("vehicleModel", offer.getVehicleModel());
                 intent.putExtra("vehicleDescription", offer.getVehicleDescription());
                 intent.putExtra("vehicleNumber", offer.getVehicleNumber());
-
+    
                 intent.putExtra("genderPreference", offer.getGenderPreference());
                 //((Activity) viewGroup.getContext()).finish();
                 viewGroup.getContext().startActivity(intent);
             }
         });
-
+        
         viewHolder.deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i("CLICK", "DELETE");
                 final AlertDialog.Builder adb = new AlertDialog.Builder(viewGroup.getContext());
-
+    
                 adb.setTitle("Delete Offer?");
                 adb.setMessage("Are you sure you want to delete your offer?");
                 adb.setNegativeButton("Cancel", null); // dismisses by default
@@ -180,25 +178,25 @@ public class OffersAdapter extends RecyclerView.Adapter<OffersAdapter.ViewHolder
                             public void onResponse(Call<OffersDatum> call, Response<OffersDatum> response) {
                                 if (response.isSuccessful()) {
                                     Log.i("DELETING: ", Integer.toString(position));
-
+    
                                     Log.i("DELETED", ": " + response.message());
                                     offers.remove(position);
                                     notifyItemRemoved(position);
                                     notifyItemRangeChanged(position, getItemCount());
-
+    
                                 } else {
                                     Log.i("ERROR_DELETE", ": " + response.message());
-
+    
                                     try {
                                         Log.i("ERROR_DELETE_MESSAGE", ": " + response.errorBody().string());
                                     } catch (IOException e) {
                                         e.printStackTrace();
                                     }
-
+    
                                 }
                                 // use response.code, response.headers, etc.
                             }
-
+    
                             @Override
                             public void onFailure(Call<OffersDatum> call, Throwable t) {
                                 // handle failure
@@ -206,10 +204,10 @@ public class OffersAdapter extends RecyclerView.Adapter<OffersAdapter.ViewHolder
                         });
                     }
                 });
-
+    
                 AlertDialog alert = adb.create();
                 alert.show();
-
+    
                 Button nbutton = alert.getButton(DialogInterface.BUTTON_NEGATIVE);
                 nbutton.setTextColor(Color.BLACK);
                 nbutton.setText("Cancel");
@@ -311,12 +309,12 @@ public class OffersAdapter extends RecyclerView.Adapter<OffersAdapter.ViewHolder
             }
         });*/
     }
-
+    
     @Override
     public int getItemCount() {
         return offers == null ? 0 : offers.size();
     }
-
+    
     class ViewHolder extends RecyclerView.ViewHolder {
         private TextView dayTextView;
         private TextView monthTextView;
@@ -324,14 +322,14 @@ public class OffersAdapter extends RecyclerView.Adapter<OffersAdapter.ViewHolder
         private TextView startingLocationTextView;
         private TextView meetUpTimeTextView;
         private TextView remarksTextView;
-
+        
         private LinearLayout linearLayout;
         private TextView seatsOfferedTextView;
         private TextView seatsLeftTextView;
         private ImageButton editButton;
         private ImageButton deleteButton;
         private TextView detailsTextView;
-
+        
         private ViewHolder(View view) {
             super(view);
             endingLocationTextView = (TextView) view.findViewById(R.id.offersItemTextViewEndingLocation);
@@ -346,10 +344,10 @@ public class OffersAdapter extends RecyclerView.Adapter<OffersAdapter.ViewHolder
             deleteButton = (ImageButton) view.findViewById(R.id.offersItemButtonDelete);
             detailsTextView = (TextView) view.findViewById(R.id.offersItemTextViewDetails);
             linearLayout = (LinearLayout) view.findViewById(R.id.offersItemLinearLayout);
-
+            
         }
     }
-
+    
     public void setOffers(List<Offer> offers) {
         this.offers = offers;
     }
