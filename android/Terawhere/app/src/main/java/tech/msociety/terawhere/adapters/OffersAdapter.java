58c779125515cb8115e278f45e93db099b937d3c
Musa <ghurabah93@hotmail.com>
2017-05-20 18:04:30 +0800

    refactor offers adapter class

--- android/Terawhere/app/src/main/java/tech/msociety/terawhere/adapters/OffersAdapter.java
+++ android/Terawhere/app/src/main/java/tech/msociety/terawhere/adapters/OffersAdapter.java
@@ -31,6 +31,28 @@ import tech.msociety.terawhere.screens.activities.CreateOfferActivity;
 import tech.msociety.terawhere.utils.DateUtils;
 
 public class OffersAdapter extends RecyclerView.Adapter<OffersAdapter.ViewHolder> {
+    public static final String DESTINATION = "Destination: ";
+    public static final String MEETING_POINT = "Meeting Point: ";
+    public static final String PICK_UP_TIME = "Pick Up Time: ";
+    public static final String REMARKS = "Remarks: ";
+    public static final String LESS_DETAILS = "\u2014 LESS DETAILS";
+    public static final String MORE_DETAILS = "+ MORE DETAILS";
+    public static final String IS_EDIT = "isEdit";
+    public static final String START_TERAWHERE_LOCATION = "startTerawhereLocation";
+    public static final String END_TERAWHERE_LOCATION = "endTerawhereLocation";
+    public static final String VEHICLE = "vehicle";
+    public static final String ID = "id";
+    public static final String DRIVER_ID = "driverId";
+    public static final String MEET_UP_TIME = "meetUpTime";
+    public static final String DRIVER_REMARKS = "driverRemarks";
+    public static final String SEATS_AVAILABLE = "seatsAvailable";
+    public static final String DELETE_OFFER = "Delete Offer?";
+    public static final String ARE_YOU_SURE_YOU_WANT_TO_DELETE_YOUR_OFFER = "Are you sure you want to delete your offer?";
+    public static final String CANCEL = "Cancel";
+    public static final String DELETE = "Delete";
+    public static final String LOG_ERROR_DELETE_MESSAGE = "ERROR_DELETE_MESSAGE";
+    public static final String CONFIRM = "Confirm";
+    public static final String TERAWHERE_PRIMARY_COLOR = "#54d8bd";
     private List<Offer> offers;
     private ViewGroup viewGroup;
 
@@ -50,12 +72,12 @@ public class OffersAdapter extends RecyclerView.Adapter<OffersAdapter.ViewHolder
         String day = DateUtils.toString(offer.getMeetupTime(), DateUtils.DAY_OF_MONTH_FORMAT);
         String month = DateUtils.toString(offer.getMeetupTime(), DateUtils.MONTH_ABBREVIATED_FORMAT);
 
-        viewHolder.endingLocationTextView.setText("Destination: " + offer.getEndTerawhereLocation().getAddress());
-        viewHolder.startingLocationTextView.setText("Meeting Point: " + offer.getStartTerawhereLocation().getAddress());
-        viewHolder.meetUpTimeTextView.setText("Pick Up Time: " + meetUpTime);
+        viewHolder.endLocationTextView.setText(DESTINATION + offer.getEndTerawhereLocation().getAddress());
+        viewHolder.startLocationTextView.setText(MEETING_POINT + offer.getStartTerawhereLocation().getAddress());
+        viewHolder.meetUpTimeTextView.setText(PICK_UP_TIME + meetUpTime);
         viewHolder.dayTextView.setText(day);
         viewHolder.monthTextView.setText(month);
-        viewHolder.remarksTextView.setText("Remarks: " + offer.getRemarks());
+        viewHolder.remarksTextView.setText(REMARKS + offer.getRemarks());
 
         final boolean[] shouldExpand = {viewHolder.remarksTextView.getVisibility() == View.GONE};
         ChangeBounds transition = new ChangeBounds();
@@ -66,11 +88,11 @@ public class OffersAdapter extends RecyclerView.Adapter<OffersAdapter.ViewHolder
             public void onClick(View v) {
                 if (shouldExpand[0]) {
                     viewHolder.remarksTextView.setVisibility(View.VISIBLE);
-                    viewHolder.detailsTextView.setText("\u2014 LESS DETAILS");
+                    viewHolder.detailsTextView.setText(LESS_DETAILS);
                     shouldExpand[0] = false;
                 } else {
                     viewHolder.remarksTextView.setVisibility(View.GONE);
-                    viewHolder.detailsTextView.setText("+ MORE DETAILS");
+                    viewHolder.detailsTextView.setText(MORE_DETAILS);
                     shouldExpand[0] = true;
                 }
 
@@ -85,12 +107,12 @@ public class OffersAdapter extends RecyclerView.Adapter<OffersAdapter.ViewHolder
             public void onClick(View v) {
                 if (shouldExpand[0]) {
                     viewHolder.remarksTextView.setVisibility(View.VISIBLE);
-                    viewHolder.detailsTextView.setText("\u2014 LESS DETAILS");
+                    viewHolder.detailsTextView.setText(LESS_DETAILS);
 
                     shouldExpand[0] = false;
                 } else {
                     viewHolder.remarksTextView.setVisibility(View.GONE);
-                    viewHolder.detailsTextView.setText("+ MORE DETAILS");
+                    viewHolder.detailsTextView.setText(MORE_DETAILS);
 
                     shouldExpand[0] = true;
                 }
@@ -101,60 +123,60 @@ public class OffersAdapter extends RecyclerView.Adapter<OffersAdapter.ViewHolder
             }
         });
 
-        viewHolder.editButton.setOnClickListener(new View.OnClickListener() {
+        viewHolder.editOfferButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-
-                TerawhereLocation startTerawhereLocation = new TerawhereLocation(offer.getStartTerawhereLocation().getName(),
+                /*********** start location ************/
+                TerawhereLocation startTerawhereLocation = new TerawhereLocation(
+                        offer.getStartTerawhereLocation().getName(),
                         offer.getStartTerawhereLocation().getAddress(),
                         offer.getStartTerawhereLocation().getLatitude(),
                         offer.getStartTerawhereLocation().getLongitude(),
                         offer.getStartTerawhereLocation().getGeohash());
 
-                TerawhereLocation endTerawhereLocation = new TerawhereLocation(offer.getEndTerawhereLocation().getName(),
+                /*********** end location ************/
+                TerawhereLocation endTerawhereLocation = new TerawhereLocation(
+                        offer.getEndTerawhereLocation().getName(),
                         offer.getEndTerawhereLocation().getAddress(),
                         offer.getEndTerawhereLocation().getLatitude(),
                         offer.getEndTerawhereLocation().getLongitude(),
                         offer.getEndTerawhereLocation().getGeohash());
 
-                Vehicle vehicle = new Vehicle(offer.getVehicle().getPlateNumber(),
+                /*********** vehicle info ************/
+                Vehicle vehicle = new Vehicle(
+                        offer.getVehicle().getPlateNumber(),
                         offer.getVehicle().getDescription(),
                         offer.getVehicle().getModel());
 
-
+                /******** store values for create offer activity *******/
                 Intent intent = new Intent(new Intent(viewGroup.getContext(), CreateOfferActivity.class));
-                intent.putExtra("startTerawhereLocation", startTerawhereLocation);
-                intent.putExtra("endTerawhereLocation", endTerawhereLocation);
-                intent.putExtra("vehicle", vehicle);
+                intent.putExtra(IS_EDIT, true);
+                intent.putExtra(START_TERAWHERE_LOCATION, startTerawhereLocation);
+                intent.putExtra(END_TERAWHERE_LOCATION, endTerawhereLocation);
+                intent.putExtra(VEHICLE, vehicle);
+                intent.putExtra(ID, offer.getOfferId());
+                intent.putExtra(DRIVER_ID, offer.getOffererId());
+                intent.putExtra(MEET_UP_TIME, offer.getMeetupTime());
+                intent.putExtra(DRIVER_REMARKS, offer.getRemarks());
+                intent.putExtra(SEATS_AVAILABLE, offer.getVacancy());
 
-
-                intent.putExtra("isEdit", true);
-                intent.putExtra("id", offer.getOfferId());
-                intent.putExtra("driverId", offer.getOffererId());
-                intent.putExtra("meetUpTime", offer.getMeetupTime());
-
-
-                intent.putExtra("driverRemarks", offer.getRemarks());
-                intent.putExtra("seatsAvailable", offer.getVacancy());
-
-
-                //((Activity) viewGroup.getContext()).finish();
+                /******** start create offer activity *******/
                 viewGroup.getContext().startActivity(intent);
             }
         });
 
-        viewHolder.deleteButton.setOnClickListener(new View.OnClickListener()
+        viewHolder.deleteOfferButton.setOnClickListener(new View.OnClickListener()
 
         {
             @Override
             public void onClick(View v) {
-                final AlertDialog.Builder adb = new AlertDialog.Builder(viewGroup.getContext());
+                final AlertDialog.Builder deleteConfirmationDialog = new AlertDialog.Builder(viewGroup.getContext());
 
-                adb.setTitle("Delete Offer?");
-                adb.setMessage("Are you sure you want to delete your offer?");
-                adb.setNegativeButton("Cancel", null); // dismisses by default
-                adb.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                deleteConfirmationDialog.setTitle(DELETE_OFFER);
+                deleteConfirmationDialog.setMessage(ARE_YOU_SURE_YOU_WANT_TO_DELETE_YOUR_OFFER);
+                deleteConfirmationDialog.setNegativeButton(CANCEL, null); // dismisses by default
+                deleteConfirmationDialog.setPositiveButton(DELETE, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         Call<Void> deleteRequest = TerawhereBackendServer.getApiInstance().deleteOffer(offers.get(position).getOfferId());
@@ -167,13 +189,11 @@ public class OffersAdapter extends RecyclerView.Adapter<OffersAdapter.ViewHolder
                                     notifyItemRangeChanged(position, getItemCount());
 
                                 } else {
-
                                     try {
-                                        Log.i("ERROR_DELETE_MESSAGE", ": " + response.errorBody().string());
+                                        Log.i(LOG_ERROR_DELETE_MESSAGE, ": " + response.errorBody().string());
                                     } catch (IOException e) {
                                         e.printStackTrace();
                                     }
-
                                 }
                             }
 
@@ -184,15 +204,15 @@ public class OffersAdapter extends RecyclerView.Adapter<OffersAdapter.ViewHolder
                     }
                 });
 
-                AlertDialog alert = adb.create();
+                AlertDialog alert = deleteConfirmationDialog.create();
                 alert.show();
 
                 Button nbutton = alert.getButton(DialogInterface.BUTTON_NEGATIVE);
                 nbutton.setTextColor(Color.BLACK);
-                nbutton.setText("Cancel");
+                nbutton.setText(CANCEL);
                 Button pbutton = alert.getButton(DialogInterface.BUTTON_POSITIVE);
-                pbutton.setTextColor(Color.parseColor("#54d8bd"));
-                pbutton.setText("Confirm");
+                pbutton.setTextColor(Color.parseColor(TERAWHERE_PRIMARY_COLOR));
+                pbutton.setText(CONFIRM);
             }
         });
     }
@@ -205,30 +225,31 @@ public class OffersAdapter extends RecyclerView.Adapter<OffersAdapter.ViewHolder
     class ViewHolder extends RecyclerView.ViewHolder {
         private TextView dayTextView;
         private TextView monthTextView;
-        private TextView endingLocationTextView;
-        private TextView startingLocationTextView;
+        private TextView endLocationTextView;
+        private TextView startLocationTextView;
         private TextView meetUpTimeTextView;
         private TextView remarksTextView;
 
         private TextView seatsOfferedTextView;
         private TextView seatsLeftTextView;
-        private ImageButton editButton;
-        private ImageButton deleteButton;
+        private ImageButton editOfferButton;
+        private ImageButton deleteOfferButton;
         private TextView detailsTextView;
 
         private RelativeLayout offerItemRelativeLayout;
+
         private ViewHolder(View view) {
             super(view);
-            endingLocationTextView = (TextView) view.findViewById(R.id.text_view_offer_end_location);
-            startingLocationTextView = (TextView) view.findViewById(R.id.text_view_offer_start_location);
+            endLocationTextView = (TextView) view.findViewById(R.id.text_view_offer_end_location);
+            startLocationTextView = (TextView) view.findViewById(R.id.text_view_offer_start_location);
             meetUpTimeTextView = (TextView) view.findViewById(R.id.text_view_offer_meet_up_time);
             seatsOfferedTextView = (TextView) view.findViewById(R.id.text_view_offer_seats_offered);
             seatsLeftTextView = (TextView) view.findViewById(R.id.text_view_offer_seats_left);
             dayTextView = (TextView) view.findViewById(R.id.text_view_offer_day);
             remarksTextView = (TextView) view.findViewById(R.id.text_view_offer_remarks);
             monthTextView = (TextView) view.findViewById(R.id.text_view_offer_month);
-            editButton = (ImageButton) view.findViewById(R.id.image_button_offer_edit);
-            deleteButton = (ImageButton) view.findViewById(R.id.image_button_offer_delete);
+            editOfferButton = (ImageButton) view.findViewById(R.id.image_button_offer_edit);
+            deleteOfferButton = (ImageButton) view.findViewById(R.id.image_button_offer_delete);
             detailsTextView = (TextView) view.findViewById(R.id.text_view_offer_view_more);
             offerItemRelativeLayout = (RelativeLayout) view.findViewById(R.id.relative_layout_offer_item);
         }
