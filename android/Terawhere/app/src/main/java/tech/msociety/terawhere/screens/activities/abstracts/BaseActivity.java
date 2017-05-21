9438c1f5d9fa81e67ee3cd086bba3988f7db9136
Faruq Rasid <me@ruqqq.sg>
2017-05-21 09:00:11 +0800

    Adjust color for the card in Recent Apps.

--- android/Terawhere/app/src/main/java/tech/msociety/terawhere/screens/activities/abstracts/BaseActivity.java
+++ android/Terawhere/app/src/main/java/tech/msociety/terawhere/screens/activities/abstracts/BaseActivity.java
@@ -1,9 +1,13 @@
 package tech.msociety.terawhere.screens.activities.abstracts;
 
 import android.Manifest;
+import android.app.ActivityManager;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
@@ -13,6 +17,7 @@ import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
+import tech.msociety.terawhere.R;
 import tech.msociety.terawhere.events.LogoutEvent;
 import tech.msociety.terawhere.globals.Constants;
 import tech.msociety.terawhere.screens.activities.FacebookLoginActivity;
@@ -59,6 +64,11 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+
+        // Set the color of Terawhere title on Recent Apps
+        Bitmap bm = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_terawhere_logo);
+        ActivityManager.TaskDescription taskDesc = new ActivityManager.TaskDescription(getString(R.string.app_name), bm, getResources().getColor(R.color.colorPrimaryDark));
+        setTaskDescription(taskDesc);
     
         if (requireNetwork) {
             if (!NetworkUtils.hasConnectivity(this)) {
