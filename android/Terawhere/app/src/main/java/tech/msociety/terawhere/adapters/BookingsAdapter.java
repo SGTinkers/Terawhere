574b8d38e5da680c32eec97a70d0190d4858cba7
Musa <ghurabah93@hotmail.com>
2017-05-21 18:46:22 +0800

    show booking details

--- android/Terawhere/app/src/main/java/tech/msociety/terawhere/adapters/BookingsAdapter.java
+++ android/Terawhere/app/src/main/java/tech/msociety/terawhere/adapters/BookingsAdapter.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import tech.msociety.terawhere.R;
 import tech.msociety.terawhere.models.Booking;
+import tech.msociety.terawhere.utils.DateUtils;
 
 public class BookingsAdapter extends RecyclerView.Adapter<BookingsAdapter.ViewHolder> {
     private List<Booking> bookings;
@@ -24,6 +25,16 @@ public class BookingsAdapter extends RecyclerView.Adapter<BookingsAdapter.ViewHo
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, int position) {
         Booking booking = bookings.get(position);
+
+        String meetUpTime = DateUtils.toFriendlyTimeString(booking.getMeetupTime());
+        String day = DateUtils.toString(booking.getMeetupTime(), DateUtils.DAY_OF_MONTH_FORMAT);
+        String month = DateUtils.toString(booking.getMeetupTime(), DateUtils.MONTH_ABBREVIATED_FORMAT);
+
+        viewHolder.endingLocationTextView.setText("Destination: " + booking.getEndTerawhereLocation().getName());
+        viewHolder.startingLocationTextView.setText("Meeting Point: " + booking.getStartTerawhereLocation().getName());
+        viewHolder.bookingDayTextView.setText(day);
+        viewHolder.bookingMonthTextView.setText(month);
+        viewHolder.bookingMeetUpTimeTextView.setText("Pick Up Time: " + meetUpTime);
 
         viewHolder.seatsBookedTextView.setText("Seats Booked: " + booking.getSeatsRemaining());
 
@@ -39,6 +50,9 @@ public class BookingsAdapter extends RecyclerView.Adapter<BookingsAdapter.ViewHo
         private TextView endingLocationTextView;
         private TextView startingLocationTextView;
         private TextView seatsBookedTextView;
+        private TextView bookingDayTextView;
+        private TextView bookingMonthTextView;
+        private TextView bookingMeetUpTimeTextView;
 
 
         private ViewHolder(View view) {
@@ -46,6 +60,9 @@ public class BookingsAdapter extends RecyclerView.Adapter<BookingsAdapter.ViewHo
             endingLocationTextView = (TextView) view.findViewById(R.id.text_view_booking_end_location);
             startingLocationTextView = (TextView) view.findViewById(R.id.text_view_booking_start_location);
             seatsBookedTextView = (TextView) view.findViewById(R.id.text_view_booking_seats_booked);
+            bookingDayTextView = (TextView) view.findViewById(R.id.text_view_booking_day);
+            bookingMonthTextView = (TextView) view.findViewById(R.id.text_view_booking_month);
+            bookingMeetUpTimeTextView = (TextView) view.findViewById(R.id.text_view_booking_meet_up_time);
         }
     }
 
