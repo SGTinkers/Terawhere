57edff62d8deb82f00536c59d551cd2bc1d6fc64
Faruq Rasid <me@ruqqq.sg>
2017-05-26 08:42:00 +0800

    Set Place Autocomplete country based on inference. Defaults to SG.

--- android/Terawhere/app/src/main/java/tech/msociety/terawhere/screens/activities/CreateOfferActivity.java
+++ android/Terawhere/app/src/main/java/tech/msociety/terawhere/screens/activities/CreateOfferActivity.java
@@ -15,6 +15,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -66,7 +67,7 @@ public class CreateOfferActivity extends ToolbarActivity {
     private Place placeStart;
     private Place placeEnd;
     private Offer offer = null;
-    
+
     private Button buttonCreateOffer;
     private TextInputEditText textInputEditTextVehicleColor;
     private TextInputEditText textInputEditTextEndLocation;
@@ -76,44 +77,44 @@ public class CreateOfferActivity extends ToolbarActivity {
     private TextInputEditText textInputEditTextVehicleModel;
     private TextInputEditText textInputEditTextMeetUpTime;
     private TextInputEditText textInputEditTextStartLocation;
-    
+
     public static Intent getIntentToStartInCreateMode(Context sourceContext) {
         Intent intent = new Intent(sourceContext, CreateOfferActivity.class);
         intent.putExtra(CreateOfferActivity.INTENT_IS_CREATE, false);
         return intent;
     }
-    
+
     public static Intent getIntentToStartInCreateModePrepopulated(Context sourceContext, Offer referenceOffer) {
         Intent intent = new Intent(sourceContext, CreateOfferActivity.class);
         intent.putExtra(CreateOfferActivity.INTENT_IS_CREATE, true);
         intent.putExtra(CreateOfferActivity.INTENT_OFFER, referenceOffer);
         return intent;
     }
-    
+
     public static Intent getIntentToStartInEditMode(Context sourceContext, Offer offerToBeEdited) {
         Intent intent = new Intent(sourceContext, CreateOfferActivity.class);
         intent.putExtra(CreateOfferActivity.INTENT_IS_EDIT, true);
         intent.putExtra(CreateOfferActivity.INTENT_OFFER, offerToBeEdited);
         return intent;
     }
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_offer);
-    
+
         trackCurrentLocation();
         initToolbar(TOOLBAR_TITLE, true);
         initViewHandles();
         initTimePickerDialog(null);
         setClickListeners();
-        
+
         Intent intent = getIntent();
         if (intent.getExtras() != null) {
             isEditOffer = intent.getExtras().getBoolean(INTENT_IS_EDIT, false);
             isCreateOffer = intent.getExtras().getBoolean(INTENT_IS_CREATE, false);
         }
-        
+
         if (isEditOffer) {
             offer = intent.getParcelableExtra(CreateOfferActivity.INTENT_OFFER);
             unloadOfferIntoUi(offer);
@@ -123,23 +124,23 @@ public class CreateOfferActivity extends ToolbarActivity {
             unloadOfferIntoUi(offer);
         }
     }
-    
+
     private void unloadOfferIntoUi(Offer offer) {
         Date meetupTime = offer.getMeetupTime();
         textInputEditTextMeetUpTime.setText(DateUtils.toFriendlyTimeString(meetupTime));
         initTimePickerDialog(meetupTime);
-    
+
         textInputEditTextStartLocation.setText(offer.getStartTerawhereLocation().getName());
         textInputEditTextEndLocation.setText(offer.getEndTerawhereLocation().getName());
         textInputEditTextSeatsAvailable.setText(String.format(Locale.getDefault(), "%d", offer.getVacancy()));
-        
+
         textInputEditTextVehiclePlateNumber.setText(offer.getVehicle().getPlateNumber());
         textInputEditTextVehicleModel.setText(offer.getVehicle().getModel());
         textInputEditTextVehicleColor.setText(offer.getVehicle().getDescription());
-    
+
         textInputEditTextRemarks.setText(offer.getRemarks());
     }
-    
+
     private void initViewHandles() {
         buttonCreateOffer = (Button) findViewById(R.id.button_create_offer);
         textInputEditTextMeetUpTime = (TextInputEditText) findViewById(R.id.text_input_edit_text_meetup_time);
@@ -151,7 +152,7 @@ public class CreateOfferActivity extends ToolbarActivity {
         textInputEditTextVehiclePlateNumber = (TextInputEditText) findViewById(R.id.text_input_edit_text_vehicle_number);
         textInputEditTextVehicleColor = (TextInputEditText) findViewById(R.id.text_input_edit_text_vehicle_color);
     }
-    
+
     private void setClickListeners() {
         textInputEditTextStartLocation.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -159,7 +160,7 @@ public class CreateOfferActivity extends ToolbarActivity {
                 callStartPlaceAutocompleteActivityIntent();
             }
         });
-        
+
         textInputEditTextEndLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -190,7 +191,7 @@ public class CreateOfferActivity extends ToolbarActivity {
                     Toast.makeText(CreateOfferActivity.this, R.string.message_required_fields, Toast.LENGTH_SHORT).show();
                     return;
                 }
-    
+
                 OfferRequestBody offerRequestBody = getOfferRequestBodyFromUi();
 
                 if (isCreateOffer) {
@@ -205,7 +206,7 @@ public class CreateOfferActivity extends ToolbarActivity {
                                 successDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                                 successDialog.setCanceledOnTouchOutside(false);
                                 successDialog.setCancelable(false);
-    
+
                                 Button okButton = (Button) successDialog.findViewById(R.id.button_ok);
                                 TextView dialogInfo = (TextView) successDialog.findViewById(R.id.text_view_successfully_created);
                                 dialogInfo.setText("Your offer has been successfully created");
@@ -213,7 +214,7 @@ public class CreateOfferActivity extends ToolbarActivity {
                                     @Override
                                     public void onClick(View v) {
                                         successDialog.dismiss();
-    
+
                                         Intent resultIntent = new Intent();
                                         resultIntent.putExtra("FirstTab", 4);
                                         setResult(RESULT_OK, resultIntent);
@@ -255,7 +256,7 @@ public class CreateOfferActivity extends ToolbarActivity {
                                     @Override
                                     public void onClick(View v) {
                                         successDialog.dismiss();
-    
+
                                         Intent resultIntent = new Intent();
                                         resultIntent.putExtra("FirstTab", 4);
                                         setResult(RESULT_OK, resultIntent);
@@ -271,7 +272,7 @@ public class CreateOfferActivity extends ToolbarActivity {
                                 }
                             }
                         }
-    
+
                         @Override
                         public void onFailure(Call<Void> call, Throwable t) {
                             TerawhereBackendServer.ErrorDatum.ToastUnknownError(CreateOfferActivity.this, t);
@@ -289,7 +290,7 @@ public class CreateOfferActivity extends ToolbarActivity {
                                 successDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                                 successDialog.setCanceledOnTouchOutside(false);
                                 successDialog.setCancelable(false);
-        
+
                                 Button okButton = (Button) successDialog.findViewById(R.id.button_ok);
                                 TextView dialogInfo = (TextView) successDialog.findViewById(R.id.text_view_successfully_created);
                                 dialogInfo.setText("Your offer has been successfully created");
@@ -297,7 +298,7 @@ public class CreateOfferActivity extends ToolbarActivity {
                                     @Override
                                     public void onClick(View v) {
                                         successDialog.dismiss();
-    
+
                                         Intent resultIntent = new Intent();
                                         resultIntent.putExtra("FirstTab", 4);
                                         setResult(RESULT_OK, resultIntent);
@@ -307,7 +308,7 @@ public class CreateOfferActivity extends ToolbarActivity {
                                 successDialog.show();
                             }
                         }
-    
+
                         @Override
                         public void onFailure(Call<Void> call, Throwable t) {
                             Log.e(TAG, "onFailure: ", t);
@@ -315,7 +316,7 @@ public class CreateOfferActivity extends ToolbarActivity {
                     });
                 }
             }
-    
+
             private OfferRequestBody getOfferRequestBodyFromUi() {
                 String startName;
                 String startAddress;
@@ -325,17 +326,17 @@ public class CreateOfferActivity extends ToolbarActivity {
                 String endAddress;
                 double endLatitude;
                 double endLongitude;
-        
+
                 startName = placeStart != null ? placeStart.getName().toString() : offer.getStartTerawhereLocation().getName();
                 startAddress = placeStart != null ? placeStart.getAddress().toString() : offer.getStartTerawhereLocation().getAddress();
                 startLatitude = placeStart != null ? placeStart.getLatLng().latitude : offer.getStartTerawhereLocation().getLatitude();
                 startLongitude = placeStart != null ? placeStart.getLatLng().longitude : offer.getStartTerawhereLocation().getLongitude();
-        
+
                 endName = placeEnd != null ? placeEnd.getName().toString() : offer.getEndTerawhereLocation().getName();
                 endAddress = placeEnd != null ? placeEnd.getAddress().toString() : offer.getEndTerawhereLocation().getAddress();
                 endLatitude = placeEnd != null ? placeEnd.getLatLng().latitude : offer.getEndTerawhereLocation().getLatitude();
                 endLongitude = placeEnd != null ? placeEnd.getLatLng().longitude : offer.getEndTerawhereLocation().getLongitude();
-        
+
                 Date dateWithTimeComponent = DateUtils.fromFriendlyTimeString(textInputEditTextMeetUpTime.getText().toString());
                 Date dateComplete = DateUtils.getDateFromDates(new Date(), dateWithTimeComponent);
 
@@ -346,7 +347,7 @@ public class CreateOfferActivity extends ToolbarActivity {
                     c.add(Calendar.DATE, 1);
                     dateComplete = c.getTime();
                 }
-        
+
                 return new OfferRequestBody(
                         DateUtils.dateToString(dateComplete, DateUtils.MYSQL_DATE_TIME_FORMAT, DateUtils.TIMEZONE_UTC),
                         startName,
@@ -366,14 +367,14 @@ public class CreateOfferActivity extends ToolbarActivity {
             }
         });
     }
-    
+
     private void initTimePickerDialog(Date meetupTime) {
         if (meetupTime == null) {
             meetupTime = new Date();
         }
-        
+
         final Calendar calendar = DateUtils.dateToCalendar(meetupTime);
-        
+
         textInputEditTextMeetUpTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -392,26 +393,37 @@ public class CreateOfferActivity extends ToolbarActivity {
             }
         });
     }
-    
+
     private void trackCurrentLocation() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
-    
+
         currentLocation = ((LocationManager) getSystemService(Context.LOCATION_SERVICE)).getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
     }
-    
+
     private boolean isFormFilled() {
         return !(textInputEditTextStartLocation.getText().toString().matches("") || textInputEditTextEndLocation.getText().toString().matches("") || textInputEditTextSeatsAvailable.getText().toString().matches("") || textInputEditTextVehicleColor.getText().toString().matches("") || textInputEditTextVehiclePlateNumber.getText().toString().matches(""));
+    }
+
+    private String getCountryCode() {
+        TelephonyManager tm = (TelephonyManager) getSystemService(getApplicationContext().TELEPHONY_SERVICE);
+        String countryCode = tm.getNetworkCountryIso();
+        if (countryCode == null) {
+            countryCode = "SG";
+        }
+
+        return countryCode.toUpperCase();
     }
     
     private void callStartPlaceAutocompleteActivityIntent() {
         LatLng minimumBound = new LatLng(currentLocation.getLatitude() - OFFSET_LATITUDE, currentLocation.getLongitude() - OFFSET_LONGITUDE);
         LatLng maximumBound = new LatLng(currentLocation.getLatitude() + OFFSET_LATITUDE, currentLocation.getLongitude() + OFFSET_LONGITUDE);
         LatLngBounds placePickerMapBounds = new LatLngBounds(minimumBound, maximumBound);
+        Log.d("Country", getCountryCode());
         try {
             AutocompleteFilter autocompleteFilter = new AutocompleteFilter.Builder()
-                    .setCountry("SG")
+                    .setCountry(getCountryCode())
                     .build();
             Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
                     .setFilter(autocompleteFilter)
@@ -429,7 +441,7 @@ public class CreateOfferActivity extends ToolbarActivity {
         LatLngBounds placePickerMapBounds = new LatLngBounds(minimumBound, maximumBound);
         try {
             AutocompleteFilter autocompleteFilter = new AutocompleteFilter.Builder()
-                    .setCountry("SG")
+                    .setCountry(getCountryCode())
                     .build();
             Intent intent =
                     new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN).setFilter(autocompleteFilter).setBoundsBias(placePickerMapBounds)
