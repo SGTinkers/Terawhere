422be9384e8895b34f70435751db7dd36463262f
Faruq Rasid <me@ruqqq.sg>
2017-05-26 08:25:57 +0800

    Launch activity when notification was created when app is in foreground.

--- android/Terawhere/app/src/main/java/tech/msociety/terawhere/services/FirebaseMessagingService.java
+++ android/Terawhere/app/src/main/java/tech/msociety/terawhere/services/FirebaseMessagingService.java
@@ -1,6 +1,8 @@
 package tech.msociety.terawhere.services;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.net.Uri;
 import android.provider.Settings;
 import android.support.v4.app.NotificationCompat;
@@ -9,6 +11,7 @@ import android.util.Log;
 import com.google.firebase.messaging.RemoteMessage;
 
 import tech.msociety.terawhere.R;
+import tech.msociety.terawhere.screens.activities.MainActivity;
 
 public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
 
@@ -17,11 +20,15 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
         super.onMessageReceived(remoteMessage);
         Log.d("FMS", "onMessageReceived: " + remoteMessage.getMessageId());
         String sound = remoteMessage.getNotification().getSound();
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
         NotificationCompat.Builder builder = new  NotificationCompat.Builder(this)
                 .setSmallIcon(R.mipmap.ic_notification)
                 .setContentTitle(remoteMessage.getNotification().getTitle())
                 .setContentText(remoteMessage.getNotification().getBody())
-                .setSound(sound != null && !sound.isEmpty() && !sound.equals("default") ? Uri.parse(sound) : Settings.System.DEFAULT_NOTIFICATION_URI);
+                .setSound(sound != null && !sound.isEmpty() && !sound.equals("default") ? Uri.parse(sound) : Settings.System.DEFAULT_NOTIFICATION_URI)
+                .setContentIntent(resultPendingIntent);
         NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         manager.notify(remoteMessage.hashCode(), builder.build());
     }
